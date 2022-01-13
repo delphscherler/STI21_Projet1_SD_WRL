5728883b9a41da5d8d164ee4d4455b5f913e2bc2
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-01-13 20:00:05 +0100

    CHange layout of admin page

--- site/html/administration.php
+++ site/html/administration.php
@@ -1,4 +1,5 @@
 <?php
+// TODO check user role
 session_start();
 //Control if user is logged in
 if(!isset($_SESSION['uid'])){
@@ -9,28 +10,43 @@ require_once __DIR__.'/helper.php';
 require_once __DIR__.'/model/entities/user.php';
 require_once __DIR__.'/action/change_password.php';
 
+$users = (new User())->findAll();
+
 require_once __DIR__.'/includes/header.php';
 ?>
 <div class="col-md-6 well">
     <h1 class="text-primary">Administration</h1>
     <hr style="border-top:1px dotted #ccc;"/>
     <button class="btn btn-dark" onclick="document.location.href='inbox.php'">Back</button>
-    <hr style="border-top: 1px dotted #ccc;"/>
-    <h2 class="text-tertiary">Update password</h2>
-    <div class="col-md-6">
-        <form method="post">
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control" required="required"/>
-            </div>
-            <button name="update" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span>Update</button>
-        </form>
-    </div>
-
-    <h2 class="text-tertiary">Manage users</h2>
     <a href="add_user.php" class="btn btn-outline-primary">Add user</a>
-    <a href="modify_user.php" class="btn btn-outline-info">Modify user</a>
-    <a href="delete_user.php" class="btn btn-outline-danger">Delete user</a>
+    <hr style="border-top: 1px dotted #ccc;"/>
+
+    <h3 class ="text-tertiary">Users</h3>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Username</th>
+            <th>Validity</th>
+            <th>Role</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($users as $id => $user): ?>
+            <tr>
+                <td><?= $user->username ?></td>
+                <td><?= $user->validity ?></td>
+                <td><?= $user->role ?></td>
+                <td>
+                    <a href="modify_passwd.php?id=<?= $user->id ?>" class="btn btn-outline-info">Change password</a>
+                    <a href="modify_user_role.php?id=<?= $user->id ?>" class="btn btn-outline-info">Change role</a>
+                    <a href="modify_user_val.php?id=<?= $user->id ?>" class="btn btn-outline-info">Change validity</a>
+                    <a href="delete_user.php?id=<?= $user->id ?>" class="btn btn-outline-danger">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
 <?php
 require_once __DIR__.'/includes/footer.php';
