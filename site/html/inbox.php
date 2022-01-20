9491f87b2a3a461d0aac957d4c8687328a88bd03
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-01-20 13:56:26 +0100

    Change inbox to use new message model

--- site/html/inbox.php
+++ site/html/inbox.php
@@ -1,94 +1,80 @@
 <?php
-	session_start();
-	
-	//Control if user is logged in
-	if(!isset($_SESSION['uid'])){
-	   header("Location:index.php");
-	}
-	
-	// Logout and close session
-	if(ISSET($_POST['logout'])){
-		session_unset();
-		session_destroy();
-		header('Location: index.php');
-	}
-	
-	//New message
-	if(ISSET($_POST['new'])) {
-		header("Location: new_message.php");
-	}
-	
-	//Administration
-	if(ISSET($_POST['admin'])) {
-		header("Location: administration.php");
-	}
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+session_start();
+require_once __DIR__.'/authorization.php';
+require_once __DIR__.'/model/entities/user.php';
+require_once __DIR__.'/model/entities/message.php';
+
+if (!STIAuthorization::access()) {
+    addFlashMessage('info', 'You need to login!');
+    redirect('inbox.php');
+}
+
+$messages = Message::getAll([
+    'receiver' => 1,
+]);
+$user = User::getById($_SESSION['uid']);
+
+// Logout and close session
+if(ISSET($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+}
+
+//New message
+if(ISSET($_POST['new'])) {
+    header("Location: new_message.php");
+}
+
+//Administration
+if(ISSET($_POST['admin'])) {
+    header("Location: administration.php");
+}
+
+require_once __DIR__.'/includes/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"/>
-    </head>
-<body>
-    <h1 class="text-primary">Inbox</h1>
-	<?php
-		$user = $_SESSION['uid'];
-		echo "<h2 class=\"text-primary\">Hello $user!</h2>";
-	?>
-	<hr style="border-top:1px dotted #ccc;"/>
-	<form method="POST">
-		<button name="new" class="btn btn-outline-primary">New message</button>
-		<button name="admin" class="btn btn-outline-info">Administration</button>
-        <a href="modify_passwd.php" class="btn btn-outline-info">Change my password</a>
-		<button name="logout" class="btn btn-outline-danger">Log out</button>
-	</form>
-	<hr style="border-top:1px dotted #ccc;"/>
-	<table class="table table-hover">
-		<thead>
-			<tr class="table-primary">
-				<th>Sender</th>
-				<th>Date</th>
-				<th>Subject</th>
-				<th>Actions</th>				
-			</tr>
-		</thead>
-		<tbody>
-		<tr>
-			<?php 			
-			   require('connexion.php');
+<h1 class="text-primary">Inbox</h1>
+<h2 class="text-primary">Hello <?= $user->username ?>!</h2>
+<hr style="border-top:1px dotted #ccc;"/>
 
-				//Select message for the current user
-				$uname=$_SESSION["username"];				
-				$sql = "SELECT * FROM messages WHERE receiver='".$uname."' ORDER BY date DESC";
-								   
-			    foreach  ($file_db->query($sql) as $row) {					
-					$sender = $row['sender'];
-					$date = $row['date'];
-					$subject = $row['subject'];
-					
-					echo "<form method=\"POST\" action=\"msg_actions.php\">";
-					echo "<tr>";
-					echo "<td>$sender</td>";
-					echo "<input type=\"hidden\" name=\"sender\" value=\"$sender\"/>"; 
-					echo "<td>$date</td>";
-					echo "<input type=\"hidden\" name=\"date\" value=\"$date\"/>";
-					echo "<td>$subject</td>";	
-					echo "<input type=\"hidden\" name=\"subject\" value=\"$subject\"/>";
-					
-					//Actions buttons					
-					echo "<td><button name=\"read\" class=\"btn btn-primary\">Read</button>";					
-					echo "<button name=\"answer\" class=\"btn btn-info\">Answer</button>";	
-					echo "<button name=\"delete\" class=\"btn btn-danger\">Delete</button></td>";	
-					echo "</tr>";
-					echo "</form>";									
-				}					
+<form method="POST">
+    <button name="new" class="btn btn-outline-primary">New message</button>
+    <?php if (STIAuthorization::access(STIAuthorization::ADMIN)): ?>
+    <button name="admin" class="btn btn-outline-info">Administration</button>
+    <?php endif; ?>
+    <a href="modify_passwd.php" class="btn btn-outline-info">Change my password</a>
+    <button name="logout" class="btn btn-outline-danger">Log out</button>
+</form>
 
-				// Close file db connection
-				$file_db = null;					
-			?>			
-		</tr>			
-		</tbody>
-</table>	
-</body>		
-</html>
+<hr style="border-top:1px dotted #ccc;"/>
+
+<table class="table table-hover">
+    <thead>
+        <tr class="table-primary">
+            <th>Sender</th>
+            <th>Date</th>
+            <th>Subject</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach (Message::getAll(['receiver' => $user->id]) as $message): ?>
+        <tr>
+            <td><?= User::getById($message->sender)->username ?></td>
+            <td><?= $message->date ?></td>
+            <td><?= $message->subject ?></td>
+            <td>
+                <a href="#read.php" class="btn btn-primary">Read</a>
+                <a href="#answer.php" class="btn btn-info">Answer</a>
+                <a href="#delete.php" class="btn btn-danger">Delete</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php
+require_once __DIR__.'/includes/footer.php';
