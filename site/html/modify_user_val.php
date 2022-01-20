8bebf8e34669e31d59acacdce8ff6df489cefa95
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-01-20 18:49:42 +0100

    Fix session

--- site/html/modify_user_val.php
+++ site/html/modify_user_val.php
@@ -1,8 +1,5 @@
 <?php
-// ini_set('display_startup_errors', 1);
-// ini_set('display_errors', 1);
-// error_reporting(-1);
-// session_start();
+session_start();
 
 require_once __DIR__.'/authorization.php';
 require_once __DIR__.'/helper.php';
